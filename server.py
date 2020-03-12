fb81cc900b28452fbf0b8d4d9042b89e343cbea6
sbibek <sbibek@hotmail.com>
2020-03-12 13:02:07 -0700

    fixed

--- server.py
+++ server.py
@@ -2,7 +2,7 @@ from scapy.all import *
 
 # first sniff for the syn request
 SYN_REQ = sniff(
-    count=1, filter="tcp and port 3333 and tcp.flags.syn==1 and tcp.flags.ack==0")
+    count=1, filter="tcp and port 3333")
 
 clientPort = SYN_REQ[0].sport
 serverPort = SYN_REQ[0].dport
@@ -13,9 +13,8 @@ ip = IP(src=serverIp, dst=clientIp)
 
 # we will reply with synack, seq = syn.seq, ack = syn.seq +1, options (mss,1460)
 SYNACK = TCP(sport=serverPort, dport=clientPort, flags="SA",
-             seq=SYN_REQ[0].seq, ack=a[0].seq + 1, options=[('MSS', 1460)])
+             seq=SYN_REQ[0].seq, ack=SYN_REQ[0].seq + 1, options=[('MSS', 1460)])
+ack_response = sr1(ip/SYNACK)
 
-ack_response = sr1(ip/TCP_SYNACK)
-
-ack.respose.show()
+ack_response.show()
 
