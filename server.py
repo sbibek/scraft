037519b4bfafde980ea1fa29b84550e5839731ee
sbibek <sbibek@hotmail.com>
2020-03-12 14:27:02 -0700

    fix

--- server.py
+++ server.py
@@ -2,7 +2,7 @@ from scapy.all import *
 
 
 def log(_from, _to, msg, seq, ack) :
-    print("{}->{} flag: {} seq: {} ack: {}".format(_from, _to, msg, seq, ack))
+    print("# {}->{} flag: {} seq: {} ack: {}".format(_from, _to, msg, seq, ack))
 
 # first sniff for the syn request
 SYN_REQ = sniff(
@@ -13,14 +13,17 @@ serverPort = SYN_REQ[0].dport
 clientIp = SYN_REQ[0][IP].src
 serverIp = SYN_REQ[0][IP].dst
 
+# log this response
 log(clientIp, serverIp, SYN_REQ[0][TCP].flags, SYN_REQ[0].seq, SYN_REQ[0].ack)
 
 ip = IP(src=serverIp, dst=clientIp)
 
+# log this packet 
+log(serverIp, clientIp, "SA", SYN_REQ[0].seq, SYN_REQ[0].seq+1) 
 # we will reply with synack, seq = syn.seq, ack = syn.seq +1, options (mss,1460)
 SYNACK = TCP(sport=serverPort, dport=clientPort, flags="SA",
              seq=SYN_REQ[0].seq, ack=SYN_REQ[0].seq + 1, options=[('MSS', 1460)])
 ack_response = sr1(ip/SYNACK)
-
-ack_response.show()
+# log this response
+log(clientIp, serverIp, ack_response[0][TCP].flags, ack_response[0].seq, ack_response[0].ack)
 
