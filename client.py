4263f379680860e6fc46ffd1d16f275943ccba44
bibek <sbibek@hotmail.com>
2020-03-16 11:01:48 -0700

    changes

--- client.py
+++ client.py
@@ -1,7 +1,7 @@
 from scapy.all import *
 import time
 
-seq = 1111
+seq = 1000
 
 dst = '134.197.42.83'
 src = '134.197.86.190'
@@ -13,7 +13,9 @@ dport = 3333
 ip = IP(src=src, dst=dst)
 # craft the syn packet
 SYN = TCP(sport=sport, dport=dport,flags='S', seq = seq)
-
 SA = sr1(ip/SYN)
+
+# now we send ACK packet
+ACK = TCP(sport=sport, dport=dport, flags='A', seq = SA[0].ack, ack=SA[0].seq + 1 )
 
 SA.show()
