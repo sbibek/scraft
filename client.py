0248dccf4f0de014aef641f0f7450c2740cfb970
bibek <sbibek@hotmail.com>
2020-03-16 11:24:01 -0700

    changes

--- client.py
+++ client.py
@@ -7,23 +7,26 @@ dst = '134.197.42.83'
 src = '134.197.86.190'
 
 # randomly select source port
-sport = random.randint(1024,65535)
+sport = random.randint(1024, 65535)
 dport = 3333
 
 ip = IP(src=src, dst=dst)
 # craft the syn packet
-SYN = TCP(sport=sport, dport=dport,flags='S', seq = seq)
+SYN = TCP(sport=sport, dport=dport, flags='S', seq=seq)
 SA = sr1(ip/SYN)
 
 # now we send ACK packet
-ACK = TCP(sport=sport, dport=dport, flags='A', seq = SA[0].ack, ack=SA[0].seq + 1 )
-sr(ip/ACK)
+ACK = TCP(sport=sport, dport=dport, flags='A',
+          seq=SA[0].ack, ack=SA[0].seq + 1)
+send(ip/ACK)
 
-PA = sniff(
-    count=1, filter="tcp and port 3333")
-
+for i in range(3):
+    PA = sniff(
+        count=1, filter="tcp and port 3333")
+    # we have to acknowledge PA packet
+    print(PA[0].load)
+    # ack = TCP(sport=sport, dport=dport, flags='A', seq = PA[0].ack, ack=PA[0].seq+)
 # PA = ACK
 # for i in range(3):
 
-
-PA.show()
+# PA.show()
